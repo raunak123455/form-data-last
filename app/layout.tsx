7fdc404a1040905b0c93342c4b2afdb297d2652e
raunak123455 <91775838+raunak123455@users.noreply.github.com>
2025-02-04 21:54:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Raunak's assignment',
+  title: 'Raunak assignment',
   description: 'assignment',
 }
 
